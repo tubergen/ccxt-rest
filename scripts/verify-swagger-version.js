5451afd89805ae46bba82d97067e7e176475aedb
Léo Galley <contact@kirkas.ch>
2023-01-27 17:26:46 -0500

    undo prettify

--- scripts/verify-swagger-version.js
+++ scripts/verify-swagger-version.js
@@ -1,33 +1,24 @@
 #!/usr/local/bin/node
 
-const fs = require("fs");
-const yaml = require("js-yaml");
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 function verify_swagger_version() {
-  const swagger_yaml = yaml.load(
-    fs.readFileSync("./api/swagger/swagger.yaml"),
-    "utf8"
-  );
-  const swagger_version = swagger_yaml.info.version;
+    const swagger_yaml = yaml.safeLoad(fs.readFileSync('./api/swagger/swagger.yaml'), 'utf8');
+    const swagger_version = swagger_yaml.info.version
 
-  const package_json = JSON.parse(fs.readFileSync("./package.json"));
-  const package_version = package_json.version;
+    const package_json = JSON.parse(fs.readFileSync('./package.json'));
+    const package_version = package_json.version
+    
+    if (package_version != swagger_version) {
+        console.info('[swagger.yaml] Version is : ' + swagger_version);
+        console.info('[package.json] Version is : ' + package_version);
 
-  if (package_version != swagger_version) {
-    console.info("[swagger.yaml] Version is : " + swagger_version);
-    console.info("[package.json] Version is : " + package_version);
-
-    console.error(
-      "******************************************************************************************************************"
-    );
-    console.error(
-      "* swagger.yaml version does NOT match package.json. Update swagger.yaml's version to match that of package.json *"
-    );
-    console.error(
-      "******************************************************************************************************************"
-    );
-    process.exit(1);
-  }
+        console.error('******************************************************************************************************************')
+        console.error('* swagger.yaml version does NOT match package.json. Update swagger.yaml\'s version to match that of package.json *')
+        console.error('******************************************************************************************************************')
+        process.exit(1)
+    }
 }
 
 verify_swagger_version();
